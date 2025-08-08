c60a33f1f3bfa7f53b38548dfabaa7b67558cb3f
Elaine <scerrielaine02@gmail.com>
2025-08-08 12:58:38 +0200

    Main Update

---
+++ GardenPatrol/Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float movespeed;
+    public float health;
+    public Rigidbody2D Body;
+    private Vector2 moveDirection;
+
+    
+
+ private void Start()
+{
+    
+}
+    // Update is called once per frame
+    void Update()
+    {
+        Processinput();
+    }
+
+     void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Processinput()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX,moveY).normalized;
+    }
+
+     private void Move()
+    {
+        {
+            Body.linearVelocity = new Vector2(moveDirection.x* movespeed,moveDirection.y*movespeed);
+        }
+    }
+}
